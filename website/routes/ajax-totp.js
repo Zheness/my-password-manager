fc9a19bd9aaf88222b358d130fb114f09cbfdaec
Max PRUDHOMME <maxprudhomme@gmail.com>
2016-04-08 10:39:42 +0530

    TOTP code received and displayed

--- website/routes/ajax-totp.js
+++ website/routes/ajax-totp.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var speakeasy = require('speakeasy');
 
 router.get('/items', function(req, res, next) {
 	res.set("Content-type", "application/json");
@@ -7,7 +8,7 @@ router.get('/items', function(req, res, next) {
 		user_id: req.session.user_id
 	};
 	req.models.Totp.find(options,
-		"_id title",
+		"_id title secret",
 		function(err, items) {
 			if (err) {
 				return res.send(JSON.stringify({
@@ -15,10 +16,21 @@ router.get('/items', function(req, res, next) {
 					"message": "No item can be fetched"
 				}));
 			}
+			var totp_items = [];
+			for (var i = 0; i < items.length; i++) {
+				totp_items.push({
+					_id: items[i]._id,
+					title: items[i].title,
+					code: speakeasy.totp({
+						secret: items[i].secret,
+						encoding: 'base32'
+					})
+				});
+			};
 			return res.send(JSON.stringify({
 				"error": false,
 				"message": "",
-				"data": items
+				"data": totp_items
 			}));
 		});
 });
