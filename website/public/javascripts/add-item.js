3589657040dff1dba70de5ec0efd74c7c39cd8e2
Max PRUDHOMME <maxprudhomme@gmail.com>
2016-04-22 09:51:20 +0530

    Fix space caracter

--- website/public/javascripts/add-item.js
+++ website/public/javascripts/add-item.js
@@ -2,7 +2,7 @@ $(function() {
 	var modal = UIkit.modal("#modalGeneratePwd");
 	var field = $(".pwdShowHideGenerate");
 	field.parent().append('&nbsp;<button id="showPwd" class="uk-button uk-button-primary" data-uk-tooltip title="Show" type="button"><span class="uk-icon-eye"></span></button>');
-	field.parent().append('&nbsp;<button id="hidePwd" class="uk-button uk-button-primary" data-uk-tooltip title="Hide" type="button"><span class="uk-icon-eye-slash"></span></button>');
+	field.parent().append('<button id="hidePwd" class="uk-button uk-button-primary" data-uk-tooltip title="Hide" type="button"><span class="uk-icon-eye-slash"></span></button>');
 	field.parent().append('&nbsp;<button id="generatePwd" class="uk-button uk-button-primary" data-uk-tooltip title="Generate" type="button"><span class="uk-icon-refresh"></span></button>');
 	var showPwd = $("#showPwd");
 	var hidePwd = $("#hidePwd");
